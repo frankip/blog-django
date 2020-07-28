f9d6ee5922ca8d64e6b713399ba810b1d8567f1d
Francky <gwadafrank@gmail.com>
2020-07-28 14:28:25 +0300

    connect to postgres database

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+class Article(models.Model):
+    title = models.CharField(max_length = 60)
+    post = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
