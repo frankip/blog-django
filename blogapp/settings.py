4e4837feaa308c71bbd8c0a3aa4965e5c51a1696
Francky <gwadafrank@gmail.com>
2020-07-29 08:41:51 +0300

    add user models

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'blogapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tribune1',
+        'USER': 'francky',
+        'PASSWORD': 'magickj'
     }
 }
 
