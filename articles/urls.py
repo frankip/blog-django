b986fbc8c17e0dae2da27ad987665f1dd35f358c
Francky <gwadafrank@gmail.com>
2020-07-29 12:28:05 +0300

    (Admin) - implement admin and search function

--- articles/urls.py
+++ articles/urls.py
@@ -2,9 +2,16 @@
 from django.conf.urls import url
 from django.urls import path
 
-from .views import welcome
+from .views import (
+    welcome, 
+    view_articles,
+    search_article
+    )
 
 urlpatterns = [
-    path('', welcome, name="welcome")
+    path('', welcome, name="welcome"),
+    path('article', view_articles, name="articles"),
+    path('search', search_article, name="search"),
+
     # url('^&')
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime as dt
+from .models import Article
 
 # Create your views here.
 
 def welcome(request):
     date = dt.datetime.today()
     return render(request, 'welcome.html', {"date": date })
+
+
+def view_articles(request):
+    article = Article.objects.all()
+
+    # print('---articles', article)
+
+    return render(request, 'articles.html', {"article": article})
+
+def search_article(request):
+
+    if 'article' in request.GET and request.GET.get('article'):
+        search = request.GET.get('article')
+        searched = Article.objects.filter(title__icontains = search)
+        message = f"{searched}"
+        context = {
+            "searched" : searched,
+        }
+        return render(request, 'search.html', context)
+
+    else:
+        message = "no search terms"
+        context = {
+            "message": message,
+        }
+        return render(request, 'search.html', context)
